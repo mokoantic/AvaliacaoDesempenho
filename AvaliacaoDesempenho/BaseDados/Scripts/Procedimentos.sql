79ae92f70bf4a139a6739b5466241372a87677df
DESKTOP-NEO1GS4\Bilas <bruno_vilela@live.com.pt>
2017-03-14 18:11:43 +0000

    Adicionadas restantes tabelas e procedimentos ao script

--- AvaliacaoDesempenho/BaseDados/Scripts/Procedimentos.sql
+++ AvaliacaoDesempenho/BaseDados/Scripts/Procedimentos.sql
@@ -255,3 +255,628 @@ WHERE
 	[Uti_ID] = @Uti_ID
 
 GO
+
+--------------------------------------------
+--          Tabela Quadrante      
+-------------------------------------------------
+
+-- Select Dynamic
+
+IF OBJECT_ID(N'[dbo].[Proc_SelectQuadrantesDynamic]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_SelectQuadrantesDynamic]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_SelectQuadrantesDynamic]
+	@WhereCondition nvarchar(500),
+	@OrderByExpression nvarchar(250) = NULL
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+DECLARE @SQL nvarchar(3250)
+
+SET @SQL = '
+SELECT
+	[Qua_Id],
+	[Qua_Descricao],
+	[Qua_Peso]
+FROM
+	[dbo].[Quadrante]
+WHERE
+	' + @WhereCondition
+
+IF @OrderByExpression IS NOT NULL AND LEN(@OrderByExpression) > 0
+BEGIN
+	SET @SQL = @SQL + '
+ORDER BY
+	' + @OrderByExpression
+END
+
+EXEC sp_executesql @SQL
+
+GO
+
+-- Insert
+
+IF OBJECT_ID(N'[dbo].[Proc_InsertQuadrante]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_InsertQuadrante]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_InsertQuadrante]
+	@Qua_Descricao nvarchar(300),
+	@Qua_Peso int,
+	@Qua_Id int OUTPUT
+AS
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[Quadrante] (
+	[Qua_Descricao],
+	[Qua_Peso]
+) VALUES (
+	@Qua_Descricao,
+	@Qua_Peso
+)
+
+SET @Qua_Id = SCOPE_IDENTITY()
+
+GO
+
+-- Update
+
+IF OBJECT_ID(N'[dbo].[Proc_UpdateQuadrante]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_UpdateQuadrante]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_UpdateQuadrante]
+	@Qua_Id int,
+	@Qua_Descricao nvarchar(300),
+	@Qua_Peso int
+AS
+
+SET NOCOUNT ON
+
+UPDATE [dbo].[Quadrante] SET
+	[Qua_Descricao] = @Qua_Descricao,
+	[Qua_Peso] = @Qua_Peso
+WHERE
+	[Qua_Id] = @Qua_Id
+
+GO
+
+-- Delete by Index
+
+IF OBJECT_ID(N'[dbo].[Proc_DeleteQuadrantesByQua_Id]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_DeleteQuadrantesByQua_Id]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_DeleteQuadrantesByQua_Id]
+	@Qua_Id int
+AS
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[Quadrante]
+WHERE
+	[Qua_Id] = @Qua_Id
+
+GO
+
+--------------------------------------------
+--          Tabela Classificacao      
+-------------------------------------------------
+
+-- Select Dynamic
+
+IF OBJECT_ID(N'[dbo].[Proc_SelectClassificacaosDynamic]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_SelectClassificacaosDynamic]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_SelectClassificacaosDynamic]
+	@WhereCondition nvarchar(500),
+	@OrderByExpression nvarchar(250) = NULL
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+DECLARE @SQL nvarchar(3250)
+
+SET @SQL = '
+SELECT
+	[Cla_Id],
+	[Cla_Qua_Id],
+	[Cla_Descricao],
+	[Cla_Cotacao]
+FROM
+	[dbo].[Classificacao]
+WHERE
+	' + @WhereCondition
+
+IF @OrderByExpression IS NOT NULL AND LEN(@OrderByExpression) > 0
+BEGIN
+	SET @SQL = @SQL + '
+ORDER BY
+	' + @OrderByExpression
+END
+
+EXEC sp_executesql @SQL
+
+GO
+
+-- Insert
+
+IF OBJECT_ID(N'[dbo].[Proc_InsertClassificacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_InsertClassificacao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_InsertClassificacao]
+	@Cla_Qua_Id int,
+	@Cla_Descricao nvarchar(300),
+	@Cla_Cotacao int,
+	@Cla_Id int OUTPUT
+AS
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[Classificacao] (
+	[Cla_Qua_Id],
+	[Cla_Descricao],
+	[Cla_Cotacao]
+) VALUES (
+	@Cla_Qua_Id,
+	@Cla_Descricao,
+	@Cla_Cotacao
+)
+
+SET @Cla_Id = SCOPE_IDENTITY()
+
+GO
+
+-- Update
+
+IF OBJECT_ID(N'[dbo].[Proc_UpdateClassificacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_UpdateClassificacao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_UpdateClassificacao]
+	@Cla_Id int,
+	@Cla_Qua_Id int,
+	@Cla_Descricao nvarchar(300),
+	@Cla_Cotacao int
+AS
+
+SET NOCOUNT ON
+
+UPDATE [dbo].[Classificacao] SET
+	[Cla_Qua_Id] = @Cla_Qua_Id,
+	[Cla_Descricao] = @Cla_Descricao,
+	[Cla_Cotacao] = @Cla_Cotacao
+WHERE
+	[Cla_Id] = @Cla_Id
+
+GO
+
+-- Delete by Index
+
+IF OBJECT_ID(N'[dbo].[Proc_DeleteClassificacaosByCla_Id]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_DeleteClassificacaosByCla_Id]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_DeleteClassificacaosByCla_Id]
+	@Cla_Id int
+AS
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[Classificacao]
+WHERE
+	[Cla_Id] = @Cla_Id
+GO
+
+--------------------------------------------
+--          Tabela Questao      
+-------------------------------------------------
+
+-- Select Dynamic
+
+IF OBJECT_ID(N'[dbo].[Proc_SelectQuestaosDynamic]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_SelectQuestaosDynamic]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_SelectQuestaosDynamic]
+	@WhereCondition nvarchar(500),
+	@OrderByExpression nvarchar(250) = NULL
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+DECLARE @SQL nvarchar(3250)
+
+SET @SQL = '
+SELECT
+	[Que_Id],
+	[Que_Qua_Id],
+	[Que_Descricao],
+	[Que_Peso]
+FROM
+	[dbo].[Questao]
+WHERE
+	' + @WhereCondition
+
+IF @OrderByExpression IS NOT NULL AND LEN(@OrderByExpression) > 0
+BEGIN
+	SET @SQL = @SQL + '
+ORDER BY
+	' + @OrderByExpression
+END
+
+EXEC sp_executesql @SQL
+
+GO
+
+-- Insert
+
+IF OBJECT_ID(N'[dbo].[Proc_InsertQuestao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_InsertQuestao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_InsertQuestao]
+	@Que_Qua_Id int,
+	@Que_Descricao nvarchar(300),
+	@Que_Peso int,
+	@Que_Id int OUTPUT
+AS
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[Questao] (
+	[Que_Qua_Id],
+	[Que_Descricao],
+	[Que_Peso]
+) VALUES (
+	@Que_Qua_Id,
+	@Que_Descricao,
+	@Que_Peso
+)
+
+SET @Que_Id = SCOPE_IDENTITY()
+
+GO
+
+-- Update
+
+IF OBJECT_ID(N'[dbo].[Proc_UpdateQuestao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_UpdateQuestao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_UpdateQuestao]
+	@Que_Id int,
+	@Que_Qua_Id int,
+	@Que_Descricao nvarchar(300),
+	@Que_Peso int
+AS
+
+SET NOCOUNT ON
+
+UPDATE [dbo].[Questao] SET
+	[Que_Qua_Id] = @Que_Qua_Id,
+	[Que_Descricao] = @Que_Descricao,
+	[Que_Peso] = @Que_Peso
+WHERE
+	[Que_Id] = @Que_Id
+
+GO
+
+-- Delete by Index
+
+IF OBJECT_ID(N'[dbo].[Proc_DeleteQuestaosByQue_Id]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_DeleteQuestaosByQue_Id]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_DeleteQuestaosByQue_Id]
+	@Que_Id int
+AS
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[Questao]
+WHERE
+	[Que_Id] = @Que_Id
+
+GO
+
+--------------------------------------------
+--          Tabela Avaliacao      
+-------------------------------------------------
+
+-- Select Dynamic
+
+IF OBJECT_ID(N'[dbo].[Proc_SelectAvaliacaosDynamic]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_SelectAvaliacaosDynamic]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_SelectAvaliacaosDynamic]
+	@WhereCondition nvarchar(500),
+	@OrderByExpression nvarchar(250) = NULL
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+DECLARE @SQL nvarchar(3250)
+
+SET @SQL = '
+SELECT
+	[Ava_Id],
+	[Ava_Uti_Id],
+	[Ava_Peso_Quadrante_Um],
+	[Ava_Peso_Quadrante_Dois],
+	[Ava_Peso_Quadrante_Tres],
+	[Ava_Peso_Quadrante_Quatro],
+	[Ava_Classificacao_Um],
+	[Ava_Classificacao_Dois],
+	[Ava_Classificacao_Tres],
+	[Ava_Classificacao_Quatro],
+	[Ava_Classificacao_Final]
+FROM
+	[dbo].[Avaliacao]
+WHERE
+	' + @WhereCondition
+
+IF @OrderByExpression IS NOT NULL AND LEN(@OrderByExpression) > 0
+BEGIN
+	SET @SQL = @SQL + '
+ORDER BY
+	' + @OrderByExpression
+END
+
+EXEC sp_executesql @SQL
+
+GO
+
+-- Insert
+
+IF OBJECT_ID(N'[dbo].[Proc_InsertAvaliacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_InsertAvaliacao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_InsertAvaliacao]
+	@Ava_Uti_Id int,
+	@Ava_Peso_Quadrante_Um int,
+	@Ava_Peso_Quadrante_Dois int,
+	@Ava_Peso_Quadrante_Tres int,
+	@Ava_Peso_Quadrante_Quatro int,
+	@Ava_Classificacao_Um int,
+	@Ava_Classificacao_Dois int,
+	@Ava_Classificacao_Tres int,
+	@Ava_Classificacao_Quatro int,
+	@Ava_Classificacao_Final int,
+	@Ava_Id int OUTPUT
+AS
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[Avaliacao] (
+	[Ava_Uti_Id],
+	[Ava_Peso_Quadrante_Um],
+	[Ava_Peso_Quadrante_Dois],
+	[Ava_Peso_Quadrante_Tres],
+	[Ava_Peso_Quadrante_Quatro],
+	[Ava_Classificacao_Um],
+	[Ava_Classificacao_Dois],
+	[Ava_Classificacao_Tres],
+	[Ava_Classificacao_Quatro],
+	[Ava_Classificacao_Final]
+) VALUES (
+	@Ava_Uti_Id,
+	@Ava_Peso_Quadrante_Um,
+	@Ava_Peso_Quadrante_Dois,
+	@Ava_Peso_Quadrante_Tres,
+	@Ava_Peso_Quadrante_Quatro,
+	@Ava_Classificacao_Um,
+	@Ava_Classificacao_Dois,
+	@Ava_Classificacao_Tres,
+	@Ava_Classificacao_Quatro,
+	@Ava_Classificacao_Final
+)
+
+SET @Ava_Id = SCOPE_IDENTITY()
+
+GO
+
+-- Update
+
+IF OBJECT_ID(N'[dbo].[Proc_UpdateAvaliacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_UpdateAvaliacao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_UpdateAvaliacao]
+	@Ava_Id int,
+	@Ava_Uti_Id int,
+	@Ava_Peso_Quadrante_Um int,
+	@Ava_Peso_Quadrante_Dois int,
+	@Ava_Peso_Quadrante_Tres int,
+	@Ava_Peso_Quadrante_Quatro int,
+	@Ava_Classificacao_Um int,
+	@Ava_Classificacao_Dois int,
+	@Ava_Classificacao_Tres int,
+	@Ava_Classificacao_Quatro int,
+	@Ava_Classificacao_Final int
+AS
+
+SET NOCOUNT ON
+
+UPDATE [dbo].[Avaliacao] SET
+	[Ava_Uti_Id] = @Ava_Uti_Id,
+	[Ava_Peso_Quadrante_Um] = @Ava_Peso_Quadrante_Um,
+	[Ava_Peso_Quadrante_Dois] = @Ava_Peso_Quadrante_Dois,
+	[Ava_Peso_Quadrante_Tres] = @Ava_Peso_Quadrante_Tres,
+	[Ava_Peso_Quadrante_Quatro] = @Ava_Peso_Quadrante_Quatro,
+	[Ava_Classificacao_Um] = @Ava_Classificacao_Um,
+	[Ava_Classificacao_Dois] = @Ava_Classificacao_Dois,
+	[Ava_Classificacao_Tres] = @Ava_Classificacao_Tres,
+	[Ava_Classificacao_Quatro] = @Ava_Classificacao_Quatro,
+	[Ava_Classificacao_Final] = @Ava_Classificacao_Final
+WHERE
+	[Ava_Id] = @Ava_Id
+
+GO
+
+-- Delete by Index
+
+IF OBJECT_ID(N'[dbo].[Proc_DeleteAvaliacaosByAva_Id]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_DeleteAvaliacaosByAva_Id]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_DeleteAvaliacaosByAva_Id]
+	@Ava_Id int
+AS
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[Avaliacao]
+WHERE
+	[Ava_Id] = @Ava_Id
+
+GO
+
+--------------------------------------------
+--          Tabela LinhaAvaliacao      
+-------------------------------------------------
+
+-- Select Dynamic
+
+IF OBJECT_ID(N'[dbo].[Proc_SelectLinhaAvaliacaosDynamic]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_SelectLinhaAvaliacaosDynamic]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_SelectLinhaAvaliacaosDynamic]
+	@WhereCondition nvarchar(500),
+	@OrderByExpression nvarchar(250) = NULL
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+DECLARE @SQL nvarchar(3250)
+
+SET @SQL = '
+SELECT
+	[LAva_Id],
+	[LAva_Ava_Id],
+	[LAva_Quadrante],
+	[LAva_PesoQuadrante],
+	[LAva_Questao],
+	[LAva_PesoQuestao],
+	[LAva_Classificacao],
+	[LAva_Pontos]
+FROM
+	[dbo].[LinhaAvaliacao]
+WHERE
+	' + @WhereCondition
+
+IF @OrderByExpression IS NOT NULL AND LEN(@OrderByExpression) > 0
+BEGIN
+	SET @SQL = @SQL + '
+ORDER BY
+	' + @OrderByExpression
+END
+
+EXEC sp_executesql @SQL
+
+GO
+
+-- Insert
+
+IF OBJECT_ID(N'[dbo].[Proc_InsertLinhaAvaliacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_InsertLinhaAvaliacao]
+GO
+
+
+CREATE PROCEDURE [dbo].[Proc_InsertLinhaAvaliacao]
+	@LAva_Ava_Id int,
+	@LAva_Quadrante nvarchar(300),
+	@LAva_PesoQuadrante int,
+	@LAva_Questao nvarchar(300),
+	@LAva_PesoQuestao int,
+	@LAva_Classificacao int,
+	@LAva_Pontos int,
+	@LAva_Id int OUTPUT
+AS
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[LinhaAvaliacao] (
+	[LAva_Ava_Id],
+	[LAva_Quadrante],
+	[LAva_PesoQuadrante],
+	[LAva_Questao],
+	[LAva_PesoQuestao],
+	[LAva_Classificacao],
+	[LAva_Pontos]
+) VALUES (
+	@LAva_Ava_Id,
+	@LAva_Quadrante,
+	@LAva_PesoQuadrante,
+	@LAva_Questao,
+	@LAva_PesoQuestao,
+	@LAva_Classificacao,
+	@LAva_Pontos
+)
+
+SET @LAva_Id = SCOPE_IDENTITY()
+
+GO
+
+-- Update
+
+IF OBJECT_ID(N'[dbo].[Proc_UpdateLinhaAvaliacao]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_UpdateLinhaAvaliacao]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_UpdateLinhaAvaliacao]
+	@LAva_Id int,
+	@LAva_Ava_Id int,
+	@LAva_Quadrante nvarchar(300),
+	@LAva_PesoQuadrante int,
+	@LAva_Questao nvarchar(300),
+	@LAva_PesoQuestao int,
+	@LAva_Classificacao int,
+	@LAva_Pontos int
+AS
+
+SET NOCOUNT ON
+
+UPDATE [dbo].[LinhaAvaliacao] SET
+	[LAva_Ava_Id] = @LAva_Ava_Id,
+	[LAva_Quadrante] = @LAva_Quadrante,
+	[LAva_PesoQuadrante] = @LAva_PesoQuadrante,
+	[LAva_Questao] = @LAva_Questao,
+	[LAva_PesoQuestao] = @LAva_PesoQuestao,
+	[LAva_Classificacao] = @LAva_Classificacao,
+	[LAva_Pontos] = @LAva_Pontos
+WHERE
+	[LAva_Id] = @LAva_Id
+
+GO
+
+-- Delete by Index
+
+IF OBJECT_ID(N'[dbo].[Proc_DeleteLinhaAvaliacaosByLAva_Id]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Proc_DeleteLinhaAvaliacaosByLAva_Id]
+GO
+
+CREATE PROCEDURE [dbo].[Proc_DeleteLinhaAvaliacaosByLAva_Id]
+	@LAva_Id int
+AS
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[LinhaAvaliacao]
+WHERE
+	[LAva_Id] = @LAva_Id
+
+GO
--- AvaliacaoDesempenho/BaseDados/Scripts/Tabelas.sql
+++ AvaliacaoDesempenho/BaseDados/Scripts/Tabelas.sql
@@ -35,3 +35,85 @@ CREATE TABLE Utilizador(
 );
 
 INSERT INTO Utilizador(Uti_Gru_Id, Uti_Login, Uti_Password, Uti_Email, Uti_EmailConfirmado) VALUES (2, 'admin', '7C-87-54-1F-D3-F3-EF-50-16-E1-2D-41-19-00-C8-7A-60-46-A8-E8', 'admin@email.com', 1)
+
+-------------------------------------
+--       Tabela Quadrante       
+-----------------------------------------
+
+CREATE TABLE Quadrante
+(
+	Qua_Id integer identity(1,1) PRIMARY KEY,
+	Qua_Descricao nvarchar(300) not null,
+	Qua_Peso integer not null
+);
+
+-------------------------------------
+--       Tabela Classificacao       
+-----------------------------------------
+
+CREATE TABLE Classificacao
+(
+	Cla_Id integer identity(1,1) PRIMARY KEY,
+	Cla_Qua_Id integer not null,
+	FOREIGN KEY (Cla_Qua_Id) REFERENCES Quadrante (Qua_Id)
+	on delete cascade
+	on update cascade,
+	Cla_Descricao nvarchar(300) not null,
+	Cla_Cotacao integer not null,
+);
+
+-------------------------------------
+--       Tabela Questao       
+-----------------------------------------
+
+CREATE TABLE Questao
+(
+	Que_Id integer identity(1,1) PRIMARY KEY,
+	Que_Qua_Id integer not null,
+	FOREIGN KEY (Que_Qua_Id) REFERENCES Quadrante (Qua_Id)
+	on delete cascade
+	on update cascade,
+	Que_Descricao nvarchar(300) not null,
+	Que_Peso integer not null,
+);
+
+-------------------------------------
+--       Tabela Avaliacao       
+-----------------------------------------
+
+CREATE TABLE Avaliacao
+(
+	Ava_Id integer identity(1,1) PRIMARY KEY,
+	Ava_Uti_Id integer not null,
+	FOREIGN KEY (Ava_Uti_Id) REFERENCES Utilizador (Uti_Id)
+	on delete cascade
+	on update cascade,
+	Ava_Peso_Quadrante_Um integer not null,
+	Ava_Peso_Quadrante_Dois integer not null,
+	Ava_Peso_Quadrante_Tres integer not null,
+	Ava_Peso_Quadrante_Quatro integer not null,
+	Ava_Classificacao_Um integer not null,
+	Ava_Classificacao_Dois integer not null,
+	Ava_Classificacao_Tres integer not null,
+	Ava_Classificacao_Quatro integer not null,
+	Ava_Classificacao_Final integer not null
+);
+
+-------------------------------------
+--       Tabela LinhaAvaliacao       
+-----------------------------------------
+
+CREATE TABLE LinhaAvaliacao
+(
+	LAva_Id integer identity(1,1) PRIMARY KEY,
+	LAva_Ava_Id integer not null,
+	FOREIGN KEY (LAva_Ava_Id) REFERENCES Avaliacao (Ava_Id)
+	on delete cascade
+	on update cascade,
+	LAva_Quadrante nvarchar(300) not null,
+	LAva_PesoQuadrante integer not null,
+	LAva_Questao nvarchar(300) not null,
+	LAva_PesoQuestao integer not null,
+	LAva_Classificacao integer not null,
+	LAva_Pontos integer not null
+);
